bd7448b66bede77932bfa01567d6e963240edc14
Administrator <Administrator@WIN-GFDKD9PSLE6>
2012-05-05 15:42:14 +0800

    lost

---
+++ war/src/main/java/com/omdasoft/orderonline/gwt/order/client/ui/MyAnchor.java
@@ -0,0 +1,13 @@
+package com.omdasoft.orderonline.gwt.order.client.ui;
+
+import com.google.gwt.user.client.ui.Anchor;
+
+public class MyAnchor extends Anchor {
+
+	public MyAnchor(String name)
+	{
+		super.setHTML("<em><i><a style=\"color:bule;\" href=\"javascript:void(0);\">"+name+"</a></i></em>");
+	}
+
+
+}
