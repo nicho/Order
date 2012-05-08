0d47a2210b86b28f28ec2dc8e1a8a74eae62f0ce
nicho <nicho.liang@china-rewards.com>
2012-05-08 11:22:23 +0800

    默认每页条数

--- war/src/main/java/com/omdasoft/orderonline/gwt/order/client/core/view/constant/ViewConstants.java
+++ war/src/main/java/com/omdasoft/orderonline/gwt/order/client/core/view/constant/ViewConstants.java
@@ -17,7 +17,7 @@ public abstract class ViewConstants {
 	public static final int per_page_number_in_dialog = 10;
 	public static final int per_page_number_in_12 = 12;
 
-	public static final int per_page_number_in_entry = 5;
+	public static final int per_page_number_in_entry = 10;
 	public static final int per_page_number_in_StaffWin = 4;
 	/**
 	 * Width of the page.
